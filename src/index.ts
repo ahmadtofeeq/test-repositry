920b71dc241cb437fea5620eb724f3231f204274
Tofeeq Ahmad <tofeeq.ahmad@bsci.com>
2022-11-10 16:41:35 +0530

    Added export files in index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './kafka/ConsumerService';
+export * from './kafka/ProducerService';
+
+export * from './kafka/KafkaConstants';
+export * from './kafka/KafkaConsumer';
+export * from './kafka/kafka.module';
+
